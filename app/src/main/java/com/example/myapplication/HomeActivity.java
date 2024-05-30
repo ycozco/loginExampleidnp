ff8c24a1a94f3670353e372d800b05d51848c6bf
Juan Sergio Zeballos Perez <jzeballosp@unsa.edu.pe>
2024-05-29 19:43:43 -0500

    asd

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,7 +2,6 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -11,17 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapplication.fragments.CuadrosFragment;
 import com.example.myapplication.fragments.HomeFragment;
 import com.example.myapplication.fragments.MapaFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
-    private Fragment fragmentManager = null;
+    private FragmentManager fragmentManager = null;
     private FragmentTransaction fragmentTransaction = null;
     private HomeFragment homeFragment = null;
     private CuadrosFragment cuadrosFragment = null;
@@ -29,6 +28,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
@@ -37,15 +37,35 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        fragmentManager = getSupportFragmentManager();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.menu_home);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.menu_home) {
+                    homeFragment = HomeFragment.newInstance("","");
+                    loadFragment(homeFragment);
+                    return true;
+                }else if(menuItem.getItemId() == R.id.menu_cuadros){
+                    cuadrosFragment = CuadrosFragment.newInstance("","");
+                    loadFragment(cuadrosFragment);
+                    return true;
+                }else if(menuItem.getItemId() == R.id.menu_mapa){
+                    mapaFragment = MapaFragment.newInstance("","");
+                    loadFragment(mapaFragment);
+                    return true;
+                }
                 return false;
             }
         });
 
+    }
+    private void loadFragment(Fragment fragment){
+        if(fragmentManager!=null){
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+            fragmentTransaction.commit();
+        }
     }
 }
